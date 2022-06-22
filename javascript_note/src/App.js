41e470bf95a61b5d3575537c2a60d05c242b4746
jaac-win-pc <jonasaacampos@gmail.com>
2022-06-22 20:38:18 -0300

    organização diretórios, criação do header e styles

---
+++ javascript_note/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import { Notification, Section } from 'rbx';
+import './App.scss';
+import Header from './components/header';
+
+
+const App = () => (
+   <Fragment>
+      <Header/>
+   </Fragment>
+)
+
+export default App;
