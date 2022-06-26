89a5e571ee4e6709ba67384bf509525c1da40d8d
jaac-win-pc <jonasaacampos@gmail.com>
2022-06-26 09:48:24 -0300

    root correction

--- javascript_note/src/App.js
+++ javascript_note/src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react";
+import { Notification, Section } from 'rbx';
 import './App.scss';
+<<<<<<< HEAD
 import AppRoutes from "./routes";
 
 
@@ -7,6 +9,15 @@ const App = () => (
    <div>
       <AppRoutes/>
    </div>
+=======
+import Header from './components/header';
+
+
+const App = () => (
+   <Fragment>
+      <Header/>
+   </Fragment>
+>>>>>>> parent of 4015000 (tela inicial criada)
 )
 
 export default App;
